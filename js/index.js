331044d6b24ce3524a7589033d26cfe867e631b3
fhh <879944211@qq.com>
2022-04-09 13:35:16 +0800

    jiguo

---
+++ js/index.js
@@ -0,0 +1,126 @@
+//倒计时
+function time() {
+    //获取当前时间
+    var now_time = new Date();
+    //创建指定时间   注意月份从0月开始  要比指定月份少一个月
+    var time = new Date(2022, 4, 1);
+    //接收时间差  得到毫秒数
+    var count_down = time - now_time;
+    //把毫秒转换为天 时  分 秒  Math.floo向下取整
+    var day = Math.floor(count_down / 1000 / 60 / 60 / 24);
+    day = day < 10 ? '0' + day : day
+    var h = Math.floor(count_down / 1000 / 60 / 60 % 24);
+    h = h < 10 ? '0' + h : h
+
+    var m = Math.floor(count_down / 1000 / 60 % 60);
+    m = m < 10 ? '0' + m : m
+
+    var s = Math.floor(count_down / 1000 % 60);
+    s = s < 10 ? '0' + s : s
+
+    var str = '申请时间剩余：' + day + '天' + h + '时' + m + '分' + s + '秒';
+    //拼接入页面
+    var div_ = document.getElementsByClassName('itme')[0];
+    div_.innerText = str;
+}
+time();
+setInterval(time, 1000);
+$(function () {
+    var ul = $('.lunbo ul');
+    //上一张
+    $('.prev').on('click', function () {
+        move();
+    })
+    //下一张
+    $('.next').on('click', function () {
+        ul.children().last().prependTo(ul); //把最后一张插入到别的第一张
+        ul.css('left', -1000);   //应为把最后一张插入到ul的第一张，当前展示的就变成第二张，所以把left改成-1000，对应到当前第二张的left值
+        ul.animate({
+            left: 0,
+        }, 800, function () {
+            ul.stop(true, true);
+        });
+    })
+    var itmer = setInterval(move, 2000);
+    //上一张点击按钮执行的函数
+    function move() {
+
+        ul.animate({
+            left: -1000,    //left移动
+        }, 800, function () {   //动画执行完成狗执行的函数
+            ul.append(ul.children().first());    //把第一张插入到ul的最后一张
+            ul.css('left', 0);   //应为把第一张插入到最后一张后，当前展示的就变成第一张，所以得把left变成0
+            ul.stop(true, true); //取消队列，避免多次点击造成糟糕的用户体验
+        })
+    }
+    //鼠标移入清楚定时器
+    ul.on('mouseover', function () {
+        console.log('a');
+        clearInterval(itmer);
+    })
+    //鼠标移出设置定时器
+    ul.on('mouseout', function () {
+        itmer = setInterval(move, 2000);
+    })
+    //请求数据
+    var index = 0;
+    $('.more-btn').on('click', function () {
+        $(this).removeClass('jz').addClass('wc');
+        var _this = $(this);
+        var url = location.origin;
+        $.ajax({
+            type: 'get',
+            url: 'js/json.js',
+            dataType: 'json',
+            success: function (data) {
+                var str = '';
+                data[index].forEach(function (ele, index) {
+                    str += ' <li>\
+                                <img src="'+ ele.img + '" alt="" width="220" height="130" >\
+                                <div class="info">\
+                                    <p class="name">'+ ele.text + '</p>\
+                                    <p class="fiex">\
+                                        <span class="left">'+ ele.price + '</span>\
+                                        <div class="right">\
+                                            <span class="one">3</span>\
+                                            <span class="two">3</span>\
+                                        </div>\
+                                    </p>\
+                                </div>\
+                            </li>';
+                })
+                $('.cool').append(str);
+                index++;
+                _this.removeClass('wc').addClass('jz');
+                if (index == 4) {
+                    _this.parent().html('<span>没有更多内容啦！！</span>');
+                }
+            },
+        })
+    })
+
+    //调用返回顶部按钮插件
+    $('#back').back({
+        offset: 'right',
+        jianju: 0
+    });
+
+    $('header .nav li a').click(function () {
+        console.log($(this));
+    })
+
+    /*显示登陆还是注册页面 */
+    block()
+    function block() {
+        let href = window.location.href.split('?')[1];
+        if (href == 'login') {
+            $('.zuce_login').css('display', 'block');
+            $('.zuce_res').css('display', 'none');
+        } else {
+            $('.zuce_res').css('display', 'block');
+            $('.zuce_login').css('display', 'none');
+        }
+    }
+})
+
+
